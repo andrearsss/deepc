72234047ad2965453cde083fd335e7bf0d524d12
andrearsss <andrears@outlook.it>
2025-07-05 15:16:42 +0200

    ReLU implemented using fmaxf

--- src/activations.c
+++ src/activations.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <activations.h>
+#include <math.h>
 
 static inline float ReLU(float x) {
     //return x * (x > 0.0f); // portable, branchless, but returns -0.0f
-    //return fmaxf(0.0f, x); // single instruction (MAXSS) on SIMD-enabled CPUs, man(3), from math.h
-    return (x > 0.0f) ? x : 0.0f;
+    //return (x > 0.0f) ? x : 0.0f; // simple, avoids -0.0f but may introduce branching
+    return fmaxf(0.0f, x); // branchless, single instruction (MAXSS) on SIMD-enabled CPUs, man(3), from math.h
 }
 
 const float (*ACTIVATIONS[])(float x) = {NULL, ReLU};
